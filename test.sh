02e22ae9c4ca1f627f6443b0baf7ee583b81d9ba
ridgegate <tom.liu@ridgegatetools.ca>
2018-12-27 21:26:30 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@
 echo "Please provide destination email for Fail2Ban Notification"
 read -p "Enter destination email, then press [ENTER] : " DEST_EMAIL
 perl -pi -e "s/f2bdestinationemail/$DEST_EMAIL/g;" /etc/fail2ban/jail.local
+echo "done"
