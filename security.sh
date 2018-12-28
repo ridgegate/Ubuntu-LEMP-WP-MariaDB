1f7a7e4fad849cd2ff9e76289e41fdd1c4d2be19
ridgegate <tom.liu@ridgegatetools.ca>
2018-12-27 16:55:27 -0800

    Update security.sh

--- security.sh
+++ security.sh
@@ -32,10 +32,10 @@ perl -pi -e "s/f2bsenderemail/$ORG_EMAIL/g;" /etc/fail2ban/jail.local
 wget https://raw.githubusercontent.com/ridgegate/Ubuntu18.04-LEMariaDBP-Wordpress-SSL-script/master/f2b-conf/nginx-http-auth.conf
 wget https://raw.githubusercontent.com/ridgegate/Ubuntu18.04-LEMariaDBP-Wordpress-SSL-script/master/f2b-conf/nginx-noscript.conf
 wget https://github.com/ridgegate/Ubuntu18.04-LEMariaDBP-Wordpress-SSL-script/blob/master/f2b-conf/wordpress.conf
-mv ./nginx-http-auth.conf /etc/fail2ban/conf.d/nginx-http-auth.conf
-mv ./nginx-noscript.conf /etc/fail2ban/conf.d/nginx-noscript.conf
-mv ./wordpress.conf /etc/fail2ban/conf.d/wordpress.conf
-sudo cp /etc/fail2ban/conf.d/apache-badbots.conf /etc/fail2ban/conf.d/nginx-badbots.conf #enable bad-bots
+mv ./nginx-http-auth.conf /etc/fail2ban/filter.d/nginx-http-auth.conf
+mv ./nginx-noscript.conf /etc/fail2ban/filter.d/nginx-noscript.conf
+mv ./wordpress.conf /etc/fail2ban/filter.d/wordpress.conf
+sudo cp /etc/fail2ban/filter.d/apache-badbots.conf /etc/fail2ban/filter.d/nginx-badbots.conf #enable bad-bots
 sudo systemctl service enable fail2ban
 sudo systemctl service start fail2ban
 echo "Done Fail2Ban"
