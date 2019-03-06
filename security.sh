70dd46847b25c9e6c05dd74860346a4a47513823
ridgegate <tom.liu@ridgegatetools.ca>
2019-03-06 12:11:57 -0800

    Update security.sh

--- security.sh
+++ security.sh
@@ -102,6 +102,7 @@ sudo apt-get install -y iptables-persistent
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 25 -j ACCEPT
 sudo iptables -A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
 sudo iptables -A INPUT -j DROP
 sudo dpkg-reconfigure iptables-persistent -u
