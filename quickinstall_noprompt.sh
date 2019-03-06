a2536f6db286c2d1971ad123be075de52fc23f64
ridgegate <tom.liu@ridgegatetools.ca>
2019-03-06 12:43:38 -0800

    Update quickinstall_noprompt.sh

--- quickinstall_noprompt.sh
+++ quickinstall_noprompt.sh
@@ -129,7 +129,7 @@ perl -pi -e "s/username_here/$dbuser/g" /var/www/html/wp-config.php
 perl -pi -e "s/password_here/$userpass/g" /var/www/html/wp-config.php
 SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
 while read -r SALT; do
-SEARCH="define('$(echo "$SALT" | cut -d "'" -f 2)"
+SEARCH="define( '$(echo "$SALT" | cut -d "'" -f 2)"
 REPLACE=$(echo "$SALT" | cut -d "'" -f 4)
 echo "Replacing: $SEARCH"
 sed -i "/^$SEARCH/s/put your unique phrase here/$(echo $REPLACE | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/" /var/www/html/wp-config.php
