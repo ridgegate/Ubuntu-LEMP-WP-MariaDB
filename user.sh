a27560460995d1a72f622455988391abf71e24a6
ridgegate <tom.liu@ridgegatetools.ca>
2019-05-27 14:06:40 -0700

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,26 +3,31 @@
 clear
 echo "Please provide a user name for the system. This prevent brute for ROOT login attempt"
 read -p "Type your system user name, then press [ENTER] : " sshuser
-echo "Password for user options:"
-echo "  * Generate Password [G]"
-echo "  * Enter own password [E]"
-echo "  * No password required [N]"
-read -p "Choose password options : " pwdoption
-
 useradd -m -s /bin/bash $sshuser
 usermod -aG sudo $sshuser
-if [[ "$pwdoption" =~ ^([gG])+$ ]]; 
-then
-  sshuserpwd=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-10)
-  echo "$sshuser:$sshuserpwd"|chpasswd
-elif [[ "$pwdoption" =~ ^([eE])+$ ]]; 
-then
-  echo "Please enter your password"
-  read -p "Please enter your password : " sshuserpwd
-  echo "$sshuser:$sshuserpwd"|chpasswd
-else
-  adduser --disabled-password --shell /bin/bash --gecos "User" $sshuser
-fi
+
+PS3="Choose password options : "
+select optpwd in "Generate Password" "Enter Password" "No Password" 
+do
+	case $optpwd in
+		"Generate Password")
+      sshuserpwd=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-10)
+      echo "$sshuser:$sshuserpwd"|chpasswd
+      break
+			;;
+		"Enter Password")
+			read -p "Please enter your password : " sshuserpwd
+      break
+			;;
+		"No Password") 
+			adduser --disabled-password --shell /bin/bash --gecos "User" $sshuser
+      break
+			;;		
+		*)		
+			echo "Error: Please try again (select 1..4)!"
+			;;		
+	esac
+done
 
 echo "Username entered"
 echo $sshuser
