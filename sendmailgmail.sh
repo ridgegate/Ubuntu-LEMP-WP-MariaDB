5a59935241379045c9f6666dd1162348a17158eb
ridgegate <tom.liu@ridgegatetools.ca>
2019-05-31 12:56:46 -0700

    Update sendmailgmail.sh

--- sendmailgmail.sh
+++ sendmailgmail.sh
@@ -15,10 +15,10 @@ apt-get update -y
 apt-get install -y sendmail mailutils sendmail-bin
 mkdir /etc/mail/authinfo
 chmod 700 /etc/mail/authinfo
-touch /etc/mail/authinfo/smtprelay
-echo "AuthInfo: \"U:F2BAlert\" \"I:$F2B_SENDER_EMAIL\" \"P:$F2B_SENDER_PASS\"" > /etc/mail/authinfo/smtprelay
-makemap hash  /etc/mail/authinfo/smtprelay <  /etc/mail/authinfo/smtprelay
-rm -f /etc/mail/authinfo/smtprelay
+touch /etc/mail/authinfo/smtpacct
+echo "AuthInfo: \"U:F2BAlert\" \"I:$F2B_SENDER_EMAIL\" \"P:$F2B_SENDER_PASS\"" > /etc/mail/authinfo/smtpacct
+makemap hash  /etc/mail/authinfo/smtpacct <  /etc/mail/authinfo/smtpacct
+rm -f /etc/mail/authinfo/smtpacct
 
 # Modify /etc/host to speed up sending
 HOSTNAMEVAR=$(</etc/hostname)
