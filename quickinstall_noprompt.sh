2cc19f0bdc012b001740fdb0ee11f911b77d9774
ridgegate <tom.liu@ridgegatetools.ca>
2020-07-15 15:43:05 -0700

    Update quickinstall_noprompt.sh

--- quickinstall_noprompt.sh
+++ quickinstall_noprompt.sh
@@ -58,7 +58,7 @@ printf '%s\n' >> /root/.secrets/cloudflare.ini
 printf '%s' 'dns_cloudflare_api_key = "' $cfapi '"'  >> /root/.secrets/cloudflare.ini
 sudo chmod 0400 /root/.secrets/cloudflare.ini
 #--Install Certbot--#
-sudo apt-get install certbot python-certbot-nginx python3-certbot-dns-cloudflare
+sudo apt-get install certbot python3-certbot-nginx python3-certbot-dns-cloudflare
 sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/cloudflare.ini -d $MY_DOMAIN,*.$MY_DOMAIN --preferred-challenges dns-01
 (crontab -l ; echo '14 5 * * * /usr/bin/certbot renew --quiet --post-hook "/usr/sbin/service nginx reload" > /dev/null 2>&1') | crontab -
 certbot renew --dry-run
