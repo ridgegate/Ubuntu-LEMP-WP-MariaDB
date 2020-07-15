e56a9e128a19edda6c35c42d390493eb141e210a
ridgegate <tom.liu@ridgegatetools.ca>
2020-07-15 16:51:35 -0700

    Update LetsEncrypt.sh

--- LetsEncrypt.sh
+++ LetsEncrypt.sh
@@ -1,9 +1,15 @@
 echo "Install LetsEncrypt SSL with CloudFlare DNS"
-echo "Please provide your CloudFlare email"
-read -p "Type your CloudFlare email, then press [ENTER] : " cfemail
+#echo "Please provide your CloudFlare email"
+#read -p "Type your CloudFlare email, then press [ENTER] : " cfemail
 echo "Please provide your CloudFlare Global API Key"
 read -p "Type your CloudFlare Global API Key, then press [ENTER] : " cfapi
 echo
+
+cfemail=wp@ridgegatetools.com
+MY_DOMAIN=sprrivets.com
+echo $cfemail
+echo $cfapi
+echo $MY_DOMAIN
 
 #---- Wildcard SSL with Cloudflare---#
 #--Create your Cloudflare Credential Files--#
@@ -13,7 +19,7 @@ printf '%s\n' >> /root/.secrets/cloudflare.ini
 printf '%s' 'dns_cloudflare_api_key = "' $cfapi '"'  >> /root/.secrets/cloudflare.ini
 sudo chmod 0400 /root/.secrets/cloudflare.ini
 #--Install Certbot--#
-sudo apt-get install certbot python3-certbot-nginx python3-certbot-dns-cloudflare
+sudo apt-get install -y certbot python3-certbot-nginx python3-certbot-dns-cloudflare
 sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/cloudflare.ini -d $MY_DOMAIN,*.$MY_DOMAIN --preferred-challenges dns-01
 (crontab -l ; echo '14 5 * * * /usr/bin/certbot renew --quiet --post-hook "/usr/sbin/service nginx reload" > /dev/null 2>&1') | crontab -
 certbot renew --dry-run
