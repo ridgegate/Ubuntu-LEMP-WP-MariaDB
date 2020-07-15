bb4591e370e981ad1c0b92ddf2267d33107b715c
ridgegate <tom.liu@ridgegatetools.ca>
2020-07-15 16:19:25 -0700

    Update quickinstall_noprompt.sh

--- quickinstall_noprompt.sh
+++ quickinstall_noprompt.sh
@@ -50,19 +50,6 @@ perl -pi -e "s/.*upload_max_filesize.*/upload_max_filesize = 200M/;" /etc/php/7.
 perl -pi -e "s/memory_limit = 128M/memory_limit = 512M/g" /etc/php/7.4/fpm/php.ini
 clear
 
-#---- Wildcard SSL with Cloudflare---#
-#--Create your Cloudflare Credential Files--#
-mkdir -p /root/.secrets/
-printf '%s' 'dns_cloudflare_email = "' $cfemail '"'  > /root/.secrets/cloudflare.ini
-printf '%s\n' >> /root/.secrets/cloudflare.ini
-printf '%s' 'dns_cloudflare_api_key = "' $cfapi '"'  >> /root/.secrets/cloudflare.ini
-sudo chmod 0400 /root/.secrets/cloudflare.ini
-#--Install Certbot--#
-sudo apt-get install certbot python3-certbot-nginx python3-certbot-dns-cloudflare
-sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/cloudflare.ini -d $MY_DOMAIN,*.$MY_DOMAIN --preferred-challenges dns-01
-(crontab -l ; echo '14 5 * * * /usr/bin/certbot renew --quiet --post-hook "/usr/sbin/service nginx reload" > /dev/null 2>&1') | crontab -
-certbot renew --dry-run
-read -t 60 -p "Please ensure certbot renewal is completed successfully and press [ENTER] to continue or [Control]+[C] to cancel"
 
 #---Editing Nginx Server Block----
 wget https://raw.githubusercontent.com/ridgegate/Ubuntu-LEMP-WP-MariaDB/master/NGINXFiles/nginx-default-block
