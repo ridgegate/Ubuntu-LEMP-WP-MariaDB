52f814938f1aa015616936a3803f84b1973e0d1d
ridgegate <tom.liu@ridgegatetools.ca>
2020-08-20 16:37:07 -0700

    Update cloudflaressl.sh

--- cloudflaressl.sh
+++ cloudflaressl.sh
@@ -8,6 +8,11 @@
 #
 #
 clear
+# Create Necessary Folders
+mkdir /etc/ssl/private
+mkdir /etc/nginx/private
+#------
+
 echo "This require Cloudflare account"
 echo "Please provide your domain name without the www. (e.g. mydomain.com)"
 read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
@@ -20,32 +25,32 @@ echo "            - Private Key Type ECDSA"
 echo "            - Any hostnames that you like"
 echo "            - Certificate Validity - Select any period you like"
 echo " **!!! DO NOT Close the following window with Private KEY until the last step !!!*****"
-echo "Copy the private key and enter below"
-read -p "Paste your private key " MY_DOMAIN_PRV_KEY
-echo $MY_DOMAIN_PRV_KEY > /etc/ssl/private/$MY_DOMAIN_key.pem
-echo 
+read -p "Copy the private key and paste into nano. Once pasted, press [CTRL]+X to exit and save the changes. Press [Enter] when ready to paste."
+touch /etc/ssl/private/${MY_DOMAIN}_key.pem
+nano /etc/ssl/private/${MY_DOMAIN}_key.pem
 echo
-echo "Thank you. Now copy the Origin Certificate section and paste it below"
-read -p "Paste your Origin Certificate key " MY_DOMAIN_ORG_CERT
-echo $MY_DOMAIN_ORG_CERT > /etc/ssl/private/$MY_DOMAIN_cert.pem
+echo "Thank you."
+read -p "Copy the certificate and paste into nano. Once pasted, press [CTRL]+X to exit and save the changes. Press [Enter] when ready to paste."
+touch /etc/ssl/private/${MY_DOMAIN}_cert.pem
+nano /etc/ssl/private/${MY_DOMAIN}_cert.pem
 echo 
 echo "Thank you. Sit tight while we change things in the background"
 echo
 echo
 
-mkdir /etc/ssl/private
-mkdir /etc/nginx/private
-
-wget https://raw.githubusercontent.com/ridgegate/Ubuntu-LEMP-WP-MariaDB/master/NGINXFiles/restrictions.conf
-mv ./restrictions.conf /etc/nginx/private/restrictions.conf
-
 SERVERIP=$(curl https://ipinfo.io/ip)
-wget -O /etc/ssl/private/cloudflareoa.pem https://support.cloudflare.com/hc/article_attachments/360037898732/origin_ca_ecc_root.pem
 
-wget https://raw.githubusercontent.com/ridgegate/Ubuntu-LEMP-WP-MariaDB/master/NGINXFiles/nginx-ssl-block
-cp -f ./nginx-ssl-block /etc/nginx/sites-available/$MY_DOMAIN
+
+wget -O /etc/nginx/private/restrictions.conf https://raw.githubusercontent.com/ridgegate/Ubuntu-LEMP-WP-MariaDB/master/NGINXFiles/restrictions.conf
+wget -O /etc/ssl/private/cloudflareoa.pem https://support.cloudflare.com/hc/article_attachments/360037898732/origin_ca_ecc_root.pem
+wget -O /etc/nginx/sites-available/$MY_DOMAIN https://raw.githubusercontent.com/ridgegate/Ubuntu-LEMP-WP-MariaDB/master/NGINXFiles/nginx-ssl-block
 perl -pi -e "s/domain.com/$MY_DOMAIN/g" /etc/nginx/sites-available/$MY_DOMAIN
 perl -pi -e "s/www.domain.com/www.$MY_DOMAIN/g" /etc/nginx/sites-available/$MY_DOMAIN
 perl -pi -e "s/publicip/$SERVERIP/g" /etc/nginx/sites-available/$MY_DOMAIN
 perl -pi -e "s/domain_directory/$MY_DOMAIN/g" /etc/nginx/sites-available/$MY_DOMAIN
+
+service nginx restart
+service php7.4-fpm restart
+service mysql restart
+
 echo "Thank you. Done!"
